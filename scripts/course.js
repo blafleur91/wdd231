75dd6aa0d11c8cbe0cd5cafcb1293aa9e157ec7d
blafleur91 <blafleur@byupathway.edu>
2026-09-04 11:28:35 -0600

    first attempt at audit tool

--- scripts/course.js
+++ scripts/course.js
@@ -77,3 +77,67 @@ const courses = [
         completed: false
     }
 ]
+
+const coursesContainer = document.querySelector(".courses");
+
+function displayCourses(courseList) {
+    // this will remove course cards, loop through the courseList, create a card, put the course information into the card, add the card to the page, 
+    // and calculate total credits.
+
+    document.querySelectorAll(".course-card").forEach(card => card.remove());
+    document.querySelectorAll(".total").forEach(total => total.remove());
+
+    courseList.forEach(course => {
+        const card = document.createElement("div");
+
+        card.classList.add("course-card");
+        card.innerHTML = `
+        <h3>${course.subject} ${course.number}</h3>
+        <p>${course.title}</p>
+        `;
+
+        if (course.completed) {
+            card.classList.add("complete");
+            // the below line of code is basically just doing appendChild without all the extra stuff to write out.
+            card.innerHTML += `<p>✓ Completed</p>`;
+        }
+
+        coursesContainer.appendChild(card);
+
+    });
+
+    // explanation from chat GPT in my own words: reduce in its most simple terminology takes items from an array and turns them into one final value, ie takes 2+2+2 into 6
+    // However, it can be anything, from string, number, object, even to another array.
+    // the below command is saying to make a variable that takes the array being input, and reduce it. it takes two parameters, in this instance total and course. course
+    // represents the item from the array, and total is the running total, the stuff getting reduced to one value.
+    // the 0 at the end means that it is the starting value. In the addition part, it is saying to take the total value and add the item from the array.
+    // the .credits is specifying to pull from the credits part of the item in the array.
+    const totalCredits = courseList.reduce(
+        (total, course) => total + course.credits,
+        0
+    );
+
+    const total = document.createElement("p");
+    total.classList.add("total");
+    total.textContent = `The total credits for courses listed above is ${totalCredits}`;
+    coursesContainer.appendChild(total);
+    
+}
+
+displayCourses(courses);
+
+// these querySelectors will run the above function with different criteria.
+document.querySelector("#all").addEventListener("click", () => {
+    displayCourses(courses);
+});
+
+document.querySelector("#cse").addEventListener("click", () => {
+    const cseCourses = courses.filter(course => course.subject === "CSE");
+    displayCourses(cseCourses);
+});
+
+document.querySelector("#wdd").addEventListener("click", () => {
+    const wddCourses = courses.filter(course => course.subject === "WDD");
+    displayCourses(wddCourses);
+});
+
